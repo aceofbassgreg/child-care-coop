f0515c4111d64f138e9eadf55e1357d662e39e32
Greg <gregory.kenenitz@gmail.com>
2016-09-10 13:02:58 -0400

    :playdates now belong_to venue, have no relationship to host family (can get host via venue)

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -1,6 +1,5 @@
 class Playdate < ActiveRecord::Base
-  has_one :venue
-  has_one :host_id #this is a family id
+  belongs_to :venue
 
   has_many :playdate_families
   has_many :families, through: :playdate_families
