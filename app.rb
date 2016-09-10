4de9e59a26c79b09dcf04dcd918c8b6c4201fad5
Greg <gregory.kenenitz@gmail.com>
2016-09-09 20:39:31 -0400

    Eliminates namespacing in app.rb (was setting up for modular app, not doing that)

--- app.rb
+++ app.rb
@@ -2,15 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments'
 
-module ChildCareCoop
-  class App < Sinatra::Application
-
-    register Sinatra::ActiveRecordExtension
-
-    get '/test' do
-      "Hello world!"
-    end
-
-  end
+get '/test' do
+  "Hello world!"
 end
 
