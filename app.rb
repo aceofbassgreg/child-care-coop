a91ea439d249dc13398d327c0eacdb71491f7dde
Greg <gregory.kenenitz@gmail.com>
2016-09-23 21:43:26 -0400

    Loads all models into app.rb, adds basic_ methods for GET, POST, and DELETE shared by all models (for now)

--- app.rb
+++ app.rb
@@ -2,124 +2,136 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments'
 
-require './app/models/address'
+Dir["./app/models/*.rb"].each do |file|
+  require file
+end
 
 ENV['RACK_ENV'] ||= 'development'
+
+def basic_get_response(model_klass)
+  model_klass.find(params[:id]).to_json
+end
+
+def basic_post_response(model_klass)
+  request_body = request.body.read
+  attrs = JSON.parse(request_body)
+  address = model_klass.create!(attrs)
+  address.to_json
+end
+
+def basic_delete_response(model_klass)
+  model_klass.find(params[:id]).destroy!
+  {'status': "#{model_klass} #{:id} has been deleted!"}
+end
 
 get '/test' do
   "Hello world!"
 end
 
 get '/address/:id' do
-  Address.find(params[:id]).to_json
+  basic_get_response(Address)
 end
 
 get '/care_request/:id' do
-  Venue.find(params[:id]).to_json
+  basic_get_response(CareRequest)
 end
 
 get '/child/:id' do
-  Child.find(params[:id]).to_json
+  basic_get_response(Child)
 end
 
 get '/credit/:id' do
-  Credit.find(params[:id]).to_json
+  basic_get_response(Credit)
 end
 
 get '/family/:id' do
-  Family.find(params[:id]).to_json
+  basic_get_response(Family)
 end
 
 get '/parent/:id' do
-  Parent.find(params[:id]).to_json
+  basic_get_response(Parent)
 end
 
 get '/playdate/:id' do
-  Playdate.find(params[:id]).to_json
+  basic_get_response(Playdate)
 end
 
 get '/playgroup/:id' do
-  Playgroup.find(params[:id]).to_json
+  basic_get_response(Playgroup)
 end
 
 get '/venue/:id' do
-  Venue.find(params[:id]).to_json
+  basic_get_response(Venue)
 end
 
 post '/address' do
-  begin
-    address = Address.create!(params[:address])
-    address.to_json
-  rescue
-    {'error': 'Could not create Address'}.to_json
-    raise 
-  end
+  basic_post_response(Address)
 end
 
-# post '/care_request' do
+post '/care_request' do
+  basic_post_response(CareRequest)
+end
 
-# end
+post '/child' do
+  basic_post_response(Child)
+end
 
-# post '/child' do
+post '/credit' do
+  basic_post_response(Credit)
+end
 
-# end
+post '/family' do
+  basic_post_response(Family)
+end
 
-# post '/credit' do
+post '/parent' do
+  basic_post_response(Parent)
+end
 
-# end
+post '/playdate' do
+  basic_post_response(Playdate)
+end
 
-# post '/family' do
+post '/playgroup' do
+  basic_post_response(Playgroup)
+end
 
-# end
-
-# post '/parent' do
-
-# end
-
-# post '/playdate' do
-
-# end
-
-# post '/playgroup' do
-
-# end
-
-# post '/venue' do
-
-# end
+post '/venue' do
+  basic_post_response(Venue)
+end
 
 delete '/address/:id' do
-
+  basic_delete_response(Address)
 end
 
-# delete '/care_request/:id' do
+delete '/care_request/:id' do
+  basic_delete_response(CareRequest)
+end
 
-# end
+delete '/child/:id' do
+  basic_delete_response(Child)
+end
 
-# delete '/child/:id' do
+delete '/credit/:id' do
+  basic_delete_response(Credit)
+end
 
-# end
+delete '/family/:id' do
+  basic_delete_response(Family)
+end
 
-# delete '/credit/:id' do
+delete '/parent/:id' do
+  basic_delete_response(Parent)
+end
 
-# end
+delete '/playdate/:id' do
+  basic_delete_response(Playdate)
+end
 
-# delete '/family/:id' do
+delete '/playgroup/:id' do
+  basic_delete_response(Playgroup)
+end
 
-# end
-
-# delete '/parent/:id' do
-
-# end
-
-# delete '/playdate/:id' do
-
-# end
-
-# delete '/playgroup/:id' do
-
-# end
-
-# delete '/venue/:id' do
-
-# end
+delete '/venue/:id' do
+  basic_delete_response(Venue)
+end
