9aba6687f70463c27f7295af5f37a4ce9a188f95
Greg <gregory.kenenitz@gmail.com>
2016-10-05 21:27:24 -0400

    Sets up model relationships btwn Playdate & Playgroup (foreign key set in earlier commit)

--- app/models/playdate.rb
+++ app/models/playdate.rb
@@ -1,5 +1,6 @@
 class Playdate < ActiveRecord::Base
   belongs_to :venue
+  belongs_to :playgroup
 
   has_many :playdate_families
   has_many :families, through: :playdate_families
--- app/models/playgroup.rb
+++ app/models/playgroup.rb
@@ -1,4 +1,5 @@
 class Playgroup < ActiveRecord::Base
   has_many :families, through: :playgroup_family
+  has_many :playdates
   validates_uniqueness_of :name
 end
